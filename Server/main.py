1d09eb2508c8c3fa0698570363bbdcdd28bf886e
Almas Zaurbekov <v-azaurbekov@microsoft.com>
2022-05-16 19:01:39 +0400

    split

--- Server/main.py
+++ Server/main.py
@@ -1,51 +1,29 @@
-def main():
-    # dict - key:value
+# input + split -> split - разделителем
+a = input().split()
 
-    order = {
-        "food": None,
-        "count": 0,
-        "price": 0
-    }
+# map -> это выполение какой-то ФУНКЦИИ на ОБЪЕКТЫ списка
+a = map(int, a)
 
-    menu = [{"food": "pizza", "count": 10, "price": 200 }, { "food":"burger", "count": 8, "price": 400 } ]
+# list -> преобразить тип данных map в список
+a = list(a)
 
-    while True:
-        command = input("Введите команду: ")
+print(a)
+# print(list(map(int, input().split())))
 
-        if command == "exit":
-            break
-        elif command == "add":
-            print(menu)
-            command_food = input("Введите еду: ")
-            command_count = int(input("Сколько штук вам положить: "))
+# 1. Получить эти имена в виде списка
+# 2. Используя цикл пробежаться по всем элементам
+# 3. Сравнить количество букв в слове
+# 4. Вывести на экран это имя, если оно больше 10 букв
 
-            item_from_menu = None
-            for item in menu:
-                if item["food"] == command_food:
-                    item_from_menu = item
-                    item["count"] -= command_count 
 
-            order["food"] = command_food
-            order["count"] = command_count
-            order["price"] = item_from_menu["price"] * command_count
+names = input().split()
+for name in names:
+    if len(name) > 10:
+        print(name)
 
-        elif command == "remove":
-            for item in menu:
-                if item["food"] == order["food"]:
-                    item["count"] += order["count"]
-                    order["food"] = None
-                    order["count"] = 0
-                    order["price"] = 0
-        
-        elif command == "show":
-            print(menu)
-            print(order)
+# 1. Надо будет получить список возрастов
+# 2. Если этот возраст кратен 2, вывести на экран True
+# 3. Если этот возраст НЕ кратен 2, вывести на экран False
 
-if __name__ == "__main__":
-    main()
-
-# 1. Запоминает названия всех перемен и функции
-# 2. Синтаксический разбор -> ошибки во время комплияции
-# 3. Запуск -> ошибка во время запуска
-# debug - отладка кода
-# refactoring - переделать логику кода
+# input: 1 2 3 4 5 6
+# output: False True False True False True
